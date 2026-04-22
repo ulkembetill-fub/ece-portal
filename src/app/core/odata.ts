4fc4d7e659fee9a285d1ed35fb00f3317466441e
Ulkem <ulkembetil@gmail.com>
2026-04-22 18:59:43 +0300

    production build tamamlandi

--- src/app/core/odata.ts
+++ src/app/core/odata.ts
@@ -1,16 +1,32 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { environment } from '../../environments/environment';
+
+export interface OdataEnvironment {
+  url: string;
+  user: string;
+  pass: string;
+}
+
+declare const require: any;
 
 @Injectable({
   providedIn: 'root'
 })
 export class OdataService {
 
-  constructor(private http: HttpClient) {}
+  private config: OdataEnvironment = { url: '', user: '', pass: '' };
+
+  constructor(private http: HttpClient) {
+    try {
+      const env = require('../../environments/environment');
+      this.config = env.environment?.odata || this.config;
+    } catch {
+      this.config = { url: '', user: '', pass: '' };
+    }
+  }
 
   private getHeaders(): HttpHeaders {
-    const creds = btoa(environment.odata.user + ':' + environment.odata.pass);
+    const creds = btoa(this.config.user + ':' + this.config.pass);
     return new HttpHeaders({
       'Authorization': 'Basic ' + creds,
       'Accept': 'application/json',
@@ -18,12 +34,12 @@ export class OdataService {
   }
 
   getTurnover(year: number) {
-    const url = `${environment.odata.url}?$filter=Year eq ${year}&$top=5000`;
+    const url = `${this.config.url}?$filter=Year eq ${year}&$top=5000`;
     return this.http.get<any>(url, { headers: this.getHeaders() });
   }
 
   getContracts() {
-    const baseUrl = environment.odata.url.replace(/\/ContractTurnoverEntry.*/, '');
+    const baseUrl = this.config.url.replace(/\/ContractTurnoverEntry.*/, '');
     const url = `${baseUrl}/ContractList?$select=No,Lot_No,Area_m2,SectorName&$top=5000`;
     return this.http.get<any>(url, { headers: this.getHeaders() });
   }
